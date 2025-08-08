0485ea1e4187409d5fb914498c8d18dfec53bb47
LS <ludddka@gmail.com>
2025-08-08 20:31:12 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/02-react-cafe/"
+  base: '/02-react-cafe/', 
   plugins: [react()],
   build: {
     sourcemap: true,
   },
-  define: {  
+  define: {
     __DEFINES__: JSON.stringify({
       SOME_FLAG: true
     })
   }
-});
+})
